c730daad989c99c6666a53bcb05adb63730fe900
Yash <ykshah@asu.edu>
2020-04-16 16:32:52 -0700

    Pushing SignUP

--- src/views/SignupView.py
+++ src/views/SignupView.py
@@ -13,18 +13,21 @@ user_schema = UserSchema()
 @signup_api.route('/', methods=['POST'])
 def signup():
     dict_body=request.get_json()
-    print(dict_body)
-    print(UserModel)
-    print(UserSchema)
     user_dict={}
+    user_dict['name']=dict_body['name']
+    user_dict['email']=dict_body['email']
+    user_dict['password']=dict_body['password']
+    user_dict['username']=dict_body['username']
+    user_dict['phone_no']=dict_body['phone_no']
+    user_dict['address']=dict_body['address']
     exist=UserModel.getcheck(dict_body['username'])
     if exist:
         return custom_response("Username already exists. Please use another one",400)
     data,err=user_schema.load(dict_body)
-
     if err:
         return custom_response("Error",400)
     user=UserModel(data)
     user.save()
     return custom_response("Success",200)
     
+
